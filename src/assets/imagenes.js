d1044ca78be5192fb525b2a2b3117ea6f615ff18
e.g.c <ironmaiden-erick@hotmail.com>
2020-12-31 00:03:57 -0600

    Empezando estilos del hero

--- src/assets/imagenes.js
+++ src/assets/imagenes.js
@@ -1,8 +1,32 @@
 import Celular from './img/celular.svg';
-
+import Css3 from './img/css3.svg';
+import Hero from './img/hero.svg';
+import Hero2 from './img/hero2.svg';
+import HTML5 from './img/html5.svg';
+import Laptop from './img/laptop.svg';
+import JS from './img/logo-javascript.svg';
+import Npm from './img/npm.svg';
+import Php from './img/php-1.svg';
+import React from './img/react.svg';
+import Sass from './img/sass-1.svg';
+import SobreMi from './img/sobre-mi.svg';
+import Tablet from './img/tablet.svg';
+import Tailwind from './img/tailwind-css.svg';
 
 
 export const imagenes = {
     imgCelular: Celular,
-
+    imgCss3: Css3,
+    imgHero: Hero,
+    imgHer2: Hero2,
+    imgHTML5: HTML5,
+    imgLaptop: Laptop,
+    imgJS: JS,
+    imgNpm: Npm,
+    imgPhp: Php,
+    imgReact: React,
+    imgSass: Sass,
+    imgSobreMi: SobreMi,
+    imgTabler: Tablet,
+    imgTailwind: Tailwind
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,19 +2,63 @@ import React from 'react';
 import styled from '@emotion/styled';
 import {imagenes} from '../assets/imagenes';
 
-const contenidoHero = styled.div`
 
 
+const ImagenHero1 = styled.img`
+    width: 20rem;
 
-`
+`;
 
+
+const ContenidoHero = styled.div`
+
+    display: grid;
+
+`;
+
+const Principal = styled.div`
+
+    border: 25px solid #9C9A9A;
+    color: #ffffff;
+    text-align: center;
+    border-radius: 20px;
+    max-width: 800px;
+    margin: 0 auto;
+    h1{
+        font-size: 3rem;
+    }
+`;
+
+const ImagenHero2 = styled.img`
+
+    width: 55rem;
+
+`;
+
+
+const texto = '</>';
 
 
 const Hero = () => {
     return ( 
-        <div>
-            <img src={imagenes.imgCelular} alt=""/>
-        </div>
+        <ContenidoHero>
+            <div>
+                <ImagenHero1 src={imagenes.imgHero} alt=""/>
+            </div>
+
+            <div className="contenedor">
+                <Principal>
+                    <h1>Erick García</h1>
+                    <h2>Desarrollador Web</h2>
+                    <p> {texto} </p>
+                </Principal>
+            </div>
+
+            <div>
+            <ImagenHero2 src={imagenes.imgHer2} alt=""/>
+            </div>
+
+        </ContenidoHero>
 
      );
 }
