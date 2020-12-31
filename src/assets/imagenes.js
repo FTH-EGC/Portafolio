baa6b4b3c285e9b0eba6e6386b687aab015d7371
e.g.c <ironmaiden-erick@hotmail.com>
2020-12-30 23:04:24 -0600

    Agregando archivo de imagenes

---
+++ src/assets/imagenes.js
@@ -0,0 +1,8 @@
+import Celular from './img/celular.svg';
+
+
+
+export const imagenes = {
+    imgCelular: Celular,
+
+}
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import {imagenes} from '../assets/imagenes';
+
+const contenidoHero = styled.div`
+
+
+
+`
+
 
 
 const Hero = () => {
     return ( 
-        <h1>Hero Aqui</h1>
+        <div>
+            <img src={imagenes.imgCelular} alt=""/>
+        </div>
 
      );
 }
